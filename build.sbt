e48ca4d7d5dd1d1b54195041108f287fb6eb54d0
Jimmy Wu <jimmywu@mit.edu>
2015-02-11 19:40:39 -0500

    dbn example pasted in

--- build.sbt
+++ build.sbt
@@ -9,4 +9,5 @@ organization := "io.prediction"
 libraryDependencies ++= Seq(
   "io.prediction"    %% "core"          % "0.8.6" % "provided",
   "org.apache.spark" %% "spark-core"    % "1.2.0" % "provided",
-  "org.apache.spark" %% "spark-mllib"   % "1.2.0" % "provided")
+  "org.apache.spark" %% "spark-mllib"   % "1.2.0" % "provided",
+  "org.deeplearning4j" % "deeplearning4j-core" % "0.0.3.2.7")
--- src/main/scala/Algorithm.scala
+++ src/main/scala/Algorithm.scala
@@ -1,13 +1,21 @@
 package org.template.vanilla
 
+import grizzled.slf4j.Logger
 import io.prediction.controller.P2LAlgorithm
 import io.prediction.controller.Params
-
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.rdd.RDD
-
-import grizzled.slf4j.Logger
+import org.apache.commons.math3.random.{MersenneTwister, RandomGenerator}
+import org.deeplearning4j.datasets.iterator.DataSetIterator
+import org.deeplearning4j.datasets.iterator.impl.IrisDataSetIterator
+import org.deeplearning4j.eval.Evaluation
+import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.dataset.DataSet
+import org.deeplearning4j.distributions.Distributions
+import org.deeplearning4j.models.classifiers.dbn.DBN
+import org.deeplearning4j.models.featuredetectors.rbm.RBM
+import org.deeplearning4j.nn.WeightInit
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration
+import org.nd4j.linalg.api.activation.Activations
+import org.nd4j.linalg.lossfunctions.LossFunctions
 
 case class AlgorithmParams(mult: Int) extends Params
 
@@ -18,6 +26,33 @@ class Algorithm(val ap: AlgorithmParams)
   @transient lazy val logger = Logger[this.type]
 
   def train(data: PreparedData): Model = {
+
+    logger.info("logging test")
+
+    // ADDED BELOW LINES
+
+    val gen: RandomGenerator = new MersenneTwister(123)
+    val conf: NeuralNetConfiguration = new NeuralNetConfiguration.Builder().iterations(100).weightInit(WeightInit.DISTRIBUTION).dist(Distributions.normal(gen, 1e-3)).constrainGradientToUnitNorm(false).lossFunction(LossFunctions.LossFunction.RECONSTRUCTION_CROSSENTROPY).activationFunction(Activations.tanh).rng(gen).visibleUnit(RBM.VisibleUnit.GAUSSIAN).hiddenUnit(RBM.HiddenUnit.RECTIFIED).dropOut(0.3f).learningRate(1e-3f).nIn(4).nOut(3).build
+    val d: DBN = new DBN.Builder().configure(conf).hiddenLayerSizes(Array[Int](3)).build
+
+    NeuralNetConfiguration.setClassifier(d.getOutputLayer.conf)
+
+    val iter: DataSetIterator = new IrisDataSetIterator(150, 150)
+
+    //fetch first
+    val next: DataSet = iter.next(110)
+    next.normalizeZeroMeanZeroUnitVariance
+
+    d.fit(next)
+
+    val eval: Evaluation = new Evaluation
+    val output: INDArray = d.output(next.getFeatureMatrix)
+    eval.eval(next.getLabels, output)
+    //log.info("Score " + eval.stats)
+    logger.info("Score " + eval.stats)
+    // END OF ADDED LINES
+
+
     // Simply count number of events
     // and multiple it by the algorithm parameter
     // and store the number as model
@@ -26,6 +61,10 @@ class Algorithm(val ap: AlgorithmParams)
   }
 
   def predict(model: Model, query: Query): PredictedResult = {
+    logger.error(s"Prediction")
+
+
+
     // Prefix the query with the model data
     val result = s"${model.mc}-${query.q}"
     PredictedResult(p = result)
